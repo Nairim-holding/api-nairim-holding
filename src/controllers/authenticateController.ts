e5c5a2a84936dfc8c9cbc7e0c553a4ceab21faca
Marcio <marciopivajunior457@gmail.com>
2025-02-26 22:43:04 -0300

    Feat: sequencia no sistema de autenticação

--- src/controllers/authenticateController.ts
+++ src/controllers/authenticateController.ts
@@ -1,20 +1,30 @@
 import { Request, Response } from "express";
+import { getUsersByEmail } from "../models/user";
 
 export class AuthenticateController {
 
-    static async login(req: Request, res: Response){
-        const {email, password} = req.body as { email: string; password: string};
-        try{
+    static async login(req: Request, res: Response): Promise<any> {
+        const { email, password } = req.body as { email: string; password: string };
 
-            if(!email || !password) {
-                res.send(400).json({status: 400, message: 'Todos os campos devem ser preenchidos.'});
+        try {
+            if (!email || !password) {
+                return res.status(400).json({ status: 400, message: 'Todos os campos devem ser preenchidos.' });
             }
 
+            const userEmail = await getUsersByEmail(email);
 
+            if( !userEmail?.email ){
+                return res.status(400).json({ status: 400, message: 'Por favor insira um email já cadastrado.' });
+            }
+
+            if( userEmail.password != password ){
+                return res.status(400).json({ status: 400, message: 'Senha incorreta.' });
+            }
+
+            return res.status(200).json({ status: 200, message: `Login realizado com sucesso! ${userEmail?.email}` });
 
         } catch (error) {
-            res.send(400).json({ status: 400, message: 'Ocorreu um erro ao fazer o login.' })
+            return res.status(500).json({ status: 500, message: 'Erro ao realizar o login.' });
         }
     }
-
 }
--- src/models/user.ts
+++ src/models/user.ts
@@ -4,6 +4,14 @@ export async function getUsers(){
     return await prisma.user.findMany();
 }
 
+export async function getUsersByEmail(email: string){
+    return await prisma.user.findUnique({ where: { email: email }});
+}
+
+export async function getUserById(id: number) {
+    return await prisma.user.findUnique({ where: { id: id }});
+}
+
 export async function createUsers(data: any) {
     const user = await prisma.user.create({
         data
