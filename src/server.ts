e6bf55130822c3b36e6ada8ab29bad843baa4c2a
Marcio <marciopivajunior457@gmail.com>
2025-06-03 12:22:51 -0300

    Feat: documentação dos endpoints

--- src/server.ts
+++ src/server.ts
@@ -3,11 +3,13 @@ import routes from "./routes";
 import prisma from "../prisma/client";
 import connectBD from "./config/db";
 import cors from 'cors';
+import path from "path";
 
 connectBD(prisma);
 
 export const app = express();
 app.use(cors());
+app.use(express.static(path.join(__dirname,'..', 'public')));
 routes(app);
 const PORT = 5000;
 
