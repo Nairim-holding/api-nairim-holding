e2be3a4cceda153dc1ced0dbdf3a2151e1a10989
Nairim-holding <nairimholding@gmail.com>
2025-06-05 11:42:34 -0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -10,12 +10,13 @@ connectBD(prisma);
 export const app = express();
 app.use(cors());
 
-app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../public', 'index.html'));
-});
 routes(app);
 const PORT = 8000;
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../public', 'index.html'));
 });
