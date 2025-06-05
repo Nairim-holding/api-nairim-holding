d0e92211c7b58dac91598ebcd695563b89e2a53b
Marcio <marciopivajunior457@gmail.com>
2025-06-05 12:17:08 -0300

    Fix: html em produção

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,24 @@
 import express from "express";
+import routes from "./routes";
+import prisma from "../prisma/client";
+import connectBD from "./config/db";
 import cors from 'cors';
 import path from "path";
 
-const app = express();
+connectBD(prisma);
 
+export const app = express();
 app.use(cors());
 
-console.log('Static files path:', path.resolve(__dirname, '../public'));
 app.use(express.static(path.resolve(__dirname, '../public')));
-
-// Rotas temporárias para teste
-app.get('/ping', (req, res) => {
-  res.send('pong');
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../public', 'index.html'));
 });
 
-const PORT = process.env.PORT || 8000;
+routes(app);
+
+const PORT = process.env.PORT  || 8000;
+
 app.listen(PORT as number, '0.0.0.0', () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
