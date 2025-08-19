95af1e3668b3a617bc7d1981eeeebe381d704676
Marcio <marciopivajunior457@gmail.com>
2025-08-18 21:34:35 -0300

    Fix: usuarios e nao alterar a senha

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -17,33 +17,40 @@ export class UserController {
         }
     }
 
+    static async getUserById(req: Request, res: Response) {
+        const { id } = req.params;
+        try {
+          const user = await getUserById(+id);
+          res.status(200).json(user);
+        } catch (error) {
+          res.status(500);
+        }
+    }
+    
+
     static async createUser(req: Request, res: Response) {
         const { name, email, password, birth_date, gender } = req.body;
         const hashedPassword = await bcrypt.hash(password, 10);
         try {
             const data = { name, email, password: hashedPassword, birth_date: new Date(birth_date), gender };
             const create = await createUsers(data);
-            res.status(200).json({ status: 200, message: `O usuário ${create.name} foi criado com sucesso!` });
+            res.status(200).json({ status: 200, message: `O Administrador ${create.name} foi criado com sucesso!` });
         } catch (error) {
             console.log(error)
-            res.status(500).json({ error: `Erro interno ao criar usuário: ${error}` });
+            res.status(500).json({ error: `Erro interno ao criar Administrador: ${error}` });
         }
     }
 
     static async updateUser(req: Request, res: Response) {
         const { id } = req.params;
-        const { name, email, password, birth_date, gender } = req.body;
+        const { name, email, birth_date, gender } = req.body;
 
         try {
-            const hashedPassword = password ? await bcrypt.hash(password, 10) : undefined;
-
-            const data: any = { name, email, birth_date, gender };
-            if (hashedPassword) data.password = hashedPassword;
-
+            const data: any = { name, email, birth_date: new Date(birth_date), gender };
             const user = await updateUser(Number(id), data);
-            res.status(200).json({ status: 200, message: `Usuário ${user.name} atualizado com sucesso.` });
+            res.status(200).json({ status: 200, message: `Administrador ${user.name} atualizado com sucesso.` });
         } catch (error) {
-            res.status(500).json({ error: `Erro ao atualizar usuário: ${error}` });
+            res.status(500).json({ error: `Erro ao atualizar Administrador: ${error}` });
         }
     }
 
@@ -52,9 +59,9 @@ export class UserController {
         const userById = await getUserById(+id);
         try {
             await deleteUsers(Number(id));
-            res.status(200).json({ status: 200, message: `Usuário ${userById?.name} foi deletado com sucesso.` });
+            res.status(200).json({ status: 200, message: `Administrador ${userById?.name} foi deletado com sucesso.` });
         } catch (error) {
-            res.status(500).json({ error: `Erro ao deletar usuário: ${error}` });
+            res.status(500).json({ error: `Erro ao deletar Administrador: ${error}` });
         }
     }
 }
